59501403cb5c0e80d25549577444777a002b9e1f
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-07-10 15:16:34 +0200

    Change sstructure for some validation inputs. Add rgex  validation for email.

--- index.js
+++ index.js
@@ -1,103 +1,79 @@
 const formSelect = document.querySelector(".form-select");
 const fullName = formSelect.querySelector("#fullname");
 const username = formSelect.querySelector("#username");
-/*const email = formSelect.querySelector("#email");
-const confirm = formSelect.querySelector("#confirm"); */
+const email = formSelect.querySelector("#email");
+/*const confirm = formSelect.querySelector("#confirm"); */
 const confirm = formSelect.querySelector("#confirm");
 const submitBtn = formSelect.querySelector("#submit-btn");
+
 
 const password = formSelect.querySelector("#password");
 let isFormValid = false;
 submitBtn.disabled = !isFormValid;
 
 document.addEventListener("keyup", () => {
-  
-  console.log("evrnt på document")
+  console.log("evrnt på document");
   // se om fullname är valid
 
+  
+  // disable button or not!!!!!!!!!
+  if (fullName.value != "" && password.value.length > 7 && username.value != "") {
+    isFormValid = true;
+    submitBtn.disabled = !isFormValid;
+  } else {
+    isFormValid = false;
+    submitBtn.disabled = !isFormValid;
+  }
 
-    
+  //se om password är valid
+});
+
+
+fullName.addEventListener("keyup", () => {
   if (fullName.value === "") {
     const errorFullname = document.querySelector("#errorFullname");
     errorFullname.textContent = "Enter fullname";
   } else {
-    errorFullname.textContent = ""
+    errorFullname.textContent = "";
   }
-
-  if(username.value === ""){
-    const errorUsername = document.querySelector("#errorUsername");
-    errorUsername.textContent = "Enter Username";
-  }
-  else{
-    errorUsername.textContent = "";
-  }
-
-  if (password.value.length < 8) {
-    password.classList.add("border-red");
-    const errorPassword = document.querySelector("#errorPassword");
-    errorPassword.textContent = "Error password fel ";
-    /* isFormValid = false;
-    submitBtn.disabled = !isFormValid; */
-  } else {
-    password.classList.remove("border-red");
-    errorPassword.textContent = "";
-    /* isFormValid = true;
-    submitBtn.disabled = !isFormValid; */
-
-    // registrationData.name = event.target.value;
-  }
-  
-
-
-// disable button or not!!!!!!!!!
-  if (fullName.value != "" && password.value.length > 7) 
-    {
-      isFormValid = true;
-      submitBtn.disabled = !isFormValid;
-  }
-  else{
-    isFormValid = false;
-      submitBtn.disabled = !isFormValid;
-  }
-  
-  //se om password är valid
 });
 
-// Error messages for inputs
+username.addEventListener("keyup", ()=>{
+  if (username.value === "") {
+    const errorUsername = document.querySelector("#errorUsername");
+    errorUsername.textContent = "Enter Username";
+  } else {
+    errorUsername.textContent = "";
+  }
+})
 
-const registrationData = {
-  name: "",
-  username: "",
-  email: "",
-  password: "",
-};
+email.addEventListener("change", () => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const isEmailValid = emailRegex.test(email.value);
 
-// Validate password min 8 char
-/* password.addEventListener("keyup", (event) => {
+   if (isEmailValid) {
+     errorEmail.textContent = "";
+     console.log('valid email')
+    } else {
+    const errorEmail = document.querySelector("#errorEmail")
+    errorEmail.textContent = "Your email is not valid."
+    console.log('not valid email')
+  }
+})
+
+password.addEventListener("keyup", (event) => {
   const password = formSelect.querySelector("#password");
   if (event.target.value.length < 8) {
     password.classList.add("border-red");
     const errorPassword = document.querySelector("#errorPassword");
-    errorPassword.textContent = "Error password fel ";
+    errorPassword.textContent = "Password must have 8 char";
   } else {
     password.classList.remove("border-red");
     errorPassword.textContent = "";
 
     registrationData.name = event.target.value;
   }
-}); */
-// set isFormValid based on fullname.value
-/* fullName.addEventListener("keyup", () => {
-  console.log("submitBtn:", submitBtn);
-  
-  if (fullName.value != "") {
-  } else {
-    const errorFullname = document.querySelector("#errorFullname");
-    errorFullname.textContent = "Enter fullname";
-  }
-}); */
-
-console.log("registrationData: ", registrationData);
+}); 
 
 formSelect.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -105,4 +81,9 @@ formSelect.addEventListener("submit", (event) => {
   // validateForm()§
 });
 
-
+const registrationData = {
+  name: "",
+  username: "",
+  email: "",
+  password: "",
+};
