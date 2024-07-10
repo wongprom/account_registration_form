717fe6e66b33e8398d91aa511067fa18dd17bc01
Jimmy Bäckström Dev <jimmybackstrom.dev@gmail.com>
2024-07-10 16:11:05 +0200

    Create span for *, disable confirm
    Create new span for * to make UI better. Input confirm is disabled when password.value.length > 8.

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ const formSelect = document.querySelector(".form-select");
 const fullName = formSelect.querySelector("#fullname");
 const username = formSelect.querySelector("#username");
 const email = formSelect.querySelector("#email");
-/*const confirm = formSelect.querySelector("#confirm"); */
 const confirm = formSelect.querySelector("#confirm");
 const submitBtn = formSelect.querySelector("#submit-btn");
-
 
 const password = formSelect.querySelector("#password");
 let isFormValid = false;
 submitBtn.disabled = !isFormValid;
 
+emailIsOk = false
+
 document.addEventListener("keyup", () => {
   console.log("evrnt på document");
   // se om fullname är valid
-
-  
+console.log("emailIsOk: ", emailIsOk)
   // disable button or not!!!!!!!!!
-  if (fullName.value != "" && password.value.length > 7 && username.value != "") {
+  if (
+    fullName.value != "" &&
+    password.value.length > 7 &&
+    username.value != "" &&
+    password.value === confirm.value &&
+    emailIsOk
+  ) {
     isFormValid = true;
     submitBtn.disabled = !isFormValid;
   } else {
@@ -28,7 +33,6 @@ document.addEventListener("keyup", () => {
   //se om password är valid
 });
 
-
 fullName.addEventListener("keyup", () => {
   if (fullName.value === "") {
     const errorFullname = document.querySelector("#errorFullname");
@@ -38,28 +42,30 @@ fullName.addEventListener("keyup", () => {
   }
 });
 
-username.addEventListener("keyup", ()=>{
+username.addEventListener("keyup", () => {
   if (username.value === "") {
     const errorUsername = document.querySelector("#errorUsername");
     errorUsername.textContent = "Enter Username";
   } else {
     errorUsername.textContent = "";
   }
-})
+});
 
 email.addEventListener("change", () => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const isEmailValid = emailRegex.test(email.value);
 
-   if (isEmailValid) {
-     errorEmail.textContent = "";
-     console.log('valid email')
-    } else {
-    const errorEmail = document.querySelector("#errorEmail")
-    errorEmail.textContent = "Your email is not valid."
-    console.log('not valid email')
+  if (isEmailValid) {
+    errorEmail.textContent = "";
+    console.log("valid email");
+    emailIsOk = true;
+  } else {
+    const errorEmail = document.querySelector("#errorEmail");
+    errorEmail.textContent = "Your email is not valid.";
+    emailIsOk = false;
+    console.log("not valid email");
   }
-})
+});
 
 password.addEventListener("keyup", (event) => {
   const password = formSelect.querySelector("#password");
@@ -67,13 +73,26 @@ password.addEventListener("keyup", (event) => {
     password.classList.add("border-red");
     const errorPassword = document.querySelector("#errorPassword");
     errorPassword.textContent = "Password must have 8 char";
+    confirm.disabled = true;
   } else {
     password.classList.remove("border-red");
     errorPassword.textContent = "";
+    confirm.disabled = false;
 
     registrationData.name = event.target.value;
   }
-}); 
+});
+
+confirm.addEventListener("change", () => {
+  console.log("password", password.value);
+  console.log("confirm", confirm.value);
+  if (password.value === confirm.value) {
+    errorConfirm.textContent = "";
+  } else {
+    const errorConfirm = document.querySelector("#errorConfirm");
+    errorConfirm.textContent = "Password do not match";
+  }
+});
 
 formSelect.addEventListener("submit", (event) => {
   event.preventDefault();
